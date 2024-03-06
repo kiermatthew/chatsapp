d4580d75898a1ac04db4402b6cbc95fb3480aee7
Kier Matthew <kiermatthew07@gmail.com>
2024-03-06 18:35:32 +0800

    updated socket url

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,10 +6,10 @@ const generateTokenAndSetCookie = (userId, res) => {
 	});
 
 	res.cookie("jwt", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
+		maxAge: 15 * 24 * 60 * 60 * 1000, 
 		httpOnly: true,
 		sameSite: "strict", 
-		secure: process.env.NODE_ENV !== "development",
+		secure: process.env.NODE_ENV !== "development"
 	});
 };
 
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chatsapp-c7xs.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
